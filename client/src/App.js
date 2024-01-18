9cc60673ffdf4f251a445f38391034a543095098
noelfoka <nomgnenoel@gmail.com>
2024-01-18 19:27:56 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route } from "react-router-dom"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Join from "./components/Join";
 import Chat from "./components/Chat";
 
 const App = () => {
     return (
         <Router>
-            <Route path="/" exact Component={Join} />
-            <Route path="/chat" Component={Chat} />
+            <Routes>
+                <Route path="/" exact Component={Join} />
+                <Route path="/chat" Component={Chat} />
+            </Routes>
         </Router>
     )
 }
--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Join = () => {
     return (
-        <div>
-            <h1>Join</h1>
-        </div>
+       <h1>Join</h1>
     )
 }
 

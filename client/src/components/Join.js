0fb3097f4f256571c1847bf20bb790ffae827170
noelfoka <nomgnenoel@gmail.com>
2024-01-19 05:29:05 +0100

    add event target

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 
 const Join = () => {
     const [name, setName] = useState('');
@@ -7,8 +8,8 @@ const Join = () => {
        <div className='joinOuterContainer'>
         <div className='joinInnerContainer'>
             <h1 className='heading'>Join</h1>
-            <div><input placeholder='Name' className='joinInput' type='text' onChange={} /></div>
-            <div><input placeholder='Room' className='joinInput mt-20' type='text' onChange={} /></div>
+            <div><input placeholder='Name' className='joinInput' type='text' onChange={(event) => setName(event.target.value)} /></div>
+            <div><input placeholder='Room' className='joinInput mt-20' type='text' onChange={(event) => setRoom(event.target.value)} /></div>
 
             <Link>
                 <button className='button mt-20' type='submit'>Sign In</button>

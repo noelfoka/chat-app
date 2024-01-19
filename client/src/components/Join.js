911fabe1216d8c925e938388e78edd5afca638bc
noelfoka <nomgnenoel@gmail.com>
2024-01-19 05:19:28 +0100

    add button to sign in

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -7,6 +7,12 @@ const Join = () => {
        <div className='joinOuterContainer'>
         <div className='joinInnerContainer'>
             <h1 className='heading'>Join</h1>
+            <div><input placeholder='' className='joinInput' type='text' onChange={} /></div>
+            <div><input placeholder='' className='joinInput' type='text' onChange={} /></div>
+
+            <Link>
+                <button className='button' type='submit'>Sign In</button>
+            </Link>
         </div>
        </div>
     )

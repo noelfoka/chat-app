ba4b24131bad98844e1d6fd7eaccd1a4f1588787
noelfoka <nomgnenoel@gmail.com>
2024-01-19 05:01:11 +0100

    add socket.on to disconnect

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    console.log('We have a new connection!!!');
+
+    socket.on('disconnect', () => {
+        console.log('We have disconnected!!!');
+    })
+})
+
 app.use(router);
 
 server.listen(PORT, () => {

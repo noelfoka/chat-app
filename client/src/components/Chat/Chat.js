1c53007c4522dc589db5b115dc8aacb06d697bc9
noelfoka <nomgnenoel@gmail.com>
2024-01-19 18:00:00 +0100

    initialize socket client

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
+let socket;
+
 const Chat = (location) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
     useEffect(() => {
         const searchTerm = location?.search;
         const { name, room } = queryString.parse(searchTerm);
+
+        socket = io();
 
         setName(name);
         setRoom(room);

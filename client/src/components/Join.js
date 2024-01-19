9fdbb7d023c8521e13c7734aa34c5cd0efb4472a
noelfoka <nomgnenoel@gmail.com>
2024-01-19 05:36:47 +0100

    add onClick to button

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -11,7 +11,7 @@ const Join = () => {
             <div><input placeholder='Name' className='joinInput' type='text' onChange={(event) => setName(event.target.value)} /></div>
             <div><input placeholder='Room' className='joinInput mt-20' type='text' onChange={(event) => setRoom(event.target.value)} /></div>
 
-            <Link>
+            <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
                 <button className='button mt-20' type='submit'>Sign In</button>
             </Link>
         </div>

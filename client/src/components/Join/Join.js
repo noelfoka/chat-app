041a8625fff89e07065e549a098bb12b804d3234
noelfoka <nomgnenoel@gmail.com>
2024-01-19 10:22:45 +0100

    import Join.css file in Join.js file

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
+import './Join.css';
+
 const Join = () => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');

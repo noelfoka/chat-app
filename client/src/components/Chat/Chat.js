394dfbda2621866917cfe399eb752da62dc2f90f
noelfoka <nomgnenoel@gmail.com>
2024-01-19 17:52:36 +0100

    add hook

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,9 +3,11 @@ import queryString from "query-string";
 import io from "socket.io-client";
 
 const Chat = (location) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
     useEffect(() => {
         const searchTerm = location.search;
-        const data = queryString.parse(searchTerm);
+        const { name, room } = queryString.parse(searchTerm);
 
         console.log(location.search);
         console.log(data);

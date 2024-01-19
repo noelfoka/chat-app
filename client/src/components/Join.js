2b7c0d2f9406947af67b7ebf95d507413c1b1fa6
noelfoka <nomgnenoel@gmail.com>
2024-01-19 05:07:03 +0100

    use useState

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
     return (
        <h1>Join</h1>
     )

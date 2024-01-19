44a85ccaa0ba86fc27810f9d6c1adae6fc7deb3a
noelfoka <nomgnenoel@gmail.com>
2024-01-19 04:29:50 +0100

    add port

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);

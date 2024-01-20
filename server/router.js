09ca0a45a71ca8d03a7ac8e8deb9f7edd44a308f
noelfoka <nomgnenoel@gmail.com>
2024-01-20 09:17:57 +0100

    update server route

--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send('Server is up and running');
-})
+  res.send({ response: "Server is up and running." }).status(200);
+});
 
 module.exports = router;
